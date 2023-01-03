445b45c5b6cd4493115f7d797c32f6d5add1518c
HeyReiner <loginreiner@hotmail.com>
2023-01-03 16:19:48 -0300

    deletando arquivos duplicados

--- Aula_24-05/05/ex02.py
+++
@@ -1,7 +0,0 @@
-# 2) Faça um algoritmo que receba um valor negativo e retorne o seu valor
-# absoluto (ex: recebe -5 e retorna 5)
-
-valor = int(input("Digite um valor qualquer: "))
-if valor < 0:
-    valor *= -1
-print("O valor absoluto deste número é {}".format(valor))
